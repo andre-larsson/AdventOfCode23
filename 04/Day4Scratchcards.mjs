74b04c27efa20392e4d0b7b1d08bb665a6dae7a9
André Larsson <larsson.andre@gmail.com>
2023-12-04 23:48:40 +0100

    skip reading of zero-length rows

--- 04/Day4Scratchcards.mjs
+++ 04/Day4Scratchcards.mjs
@@ -1,16 +1,17 @@
 import { readInput } from "../shared/util.mjs"
 
 
+const input = await readInput("input.data");
+
 // Part 1 and 2
 const findNumElfWins = (row) => {
-    if(!row) return 0;
     const [wNumStr, eNumStr] = row.replace(/Card.+\d+:/,"").split("|");
     let wNums = wNumStr.match(/(\d+)/g) || [];
     let eNums = eNumStr.match(/(\d+)/g) || [];
     
     // remove duplicates (needed?)
-    wNums = Array.from(new Set(wNums));
-    eNums = Array.from(new Set(eNums));
+    // wNums = Array.from(new Set(wNums));
+    // eNums = Array.from(new Set(eNums));
     
     return (eNums.filter((e) => wNums.includes(e))).length;
 }
@@ -20,6 +21,7 @@ let pointsTable = {}; // cardNum: numPoints (for part 2)
 const getPoints = (cardString) => {
     const result = cardString.split("\n")
 	  .map((row, y) => {
+	      if(row.length<1) return 0;
 	      const numElfWins = findNumElfWins(row);
 	      pointsTable[y+1] = numElfWins;
 	      if(numElfWins==0) return 0;
@@ -57,16 +59,19 @@ const getNumCards = (cardString) => {
     if(Object.keys(pointsTable).length ==0){
 	console.log("No stored card points! Run getPoints() to fill in the points table!");
     }
-    const n = (cardString.split("\n")).length
+
+    // number of cards
+    const nCards = (cardString.split("\n")).reduce((agg, row) =>{
+	if(row.length>0) return agg + 1; // count rows with non-zero length
+	else return agg;
+    }, 0);
+
     let sum = 0;
-    for(let i=0; i<n; i++){
+    for(let i=0; i<nCards; i++){
 	sum += sumNumCardWins(i+1);
     }
     return sum;
 }
 
-const input = await readInput("sample.data");
 console.log("Part 1: ", getPoints(input));
-
-// TODO: wrong answer on part two (correct on example data)
-// console.log("Part 2: ", getNumCards(input));
+console.log("Part 2: ", getNumCards(input));
