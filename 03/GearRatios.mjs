5aaaa682cd94d5a72ac0e1447c2aad9915306a31
André Larsson <larsson.andre@gmail.com>
2023-12-04 18:36:00 +0100

    added solution for day 3, part 2

--- 03/GearRatios.mjs
+++ 03/GearRatios.mjs
@@ -53,12 +53,51 @@ let sum2=0;
 // x width, y height
 for(let y=0; y<lineArray.length; y++){
     const line = lineArray[y];
-    const matches = line.matchAll(/\*/g);
+    const starMatches = line.matchAll(/\*/g);
 
-    for(const match of matches){
-	// TODO
+    for(const sm of starMatches){
+	// find adjacent number to the left (if any)
+	let adjacentNum = [];
+	
+	if(sm.index>0){
+	    const leftNum = line.slice(0, sm.index).match(/(\d+)$/)
+	    if(leftNum) adjacentNum.push(leftNum[1]);
+	}
+
+	// find adjacent number to the right
+	if(sm.index<lineWidth-1){
+	    const rightNum = line.slice(sm.index+1, -1).match(/^(\d+)/);
+	    if(rightNum) adjacentNum.push(rightNum[1]);
+	}
+
+	// find adjacent above
+	if(y>0){
+	    let numbersAbove = [...lineArray[y-1].matchAll(/(\d+)/g)]
+		.filter((numMatch) => {
+		    const startI = (numMatch.index-1);
+		    const endI = (numMatch.index + numMatch[1].length);
+		    return ( sm.index >= startI && sm.index <=endI);
+		})
+		.forEach((m) =>{
+		    adjacentNum.push(m[1])
+		});
+	}
+
+	// find adjacent below
+	if(y<lineArray.length-1){
+	    let numbersBelow = [...lineArray[y+1].matchAll(/(\d+)/g)]
+		.filter((numMatch) => {
+		    const startI = (numMatch.index-1);
+		    const endI = (numMatch.index + numMatch[1].length);
+		    return ( sm.index >= startI && sm.index <=endI);
+		})
+		.forEach((m) =>{
+		    adjacentNum.push(m[1])
+		});
+	}
+
+	if(adjacentNum.length == 2) sum2 += Number(adjacentNum[0])*Number(adjacentNum[1]);
     }
 }
 
 console.log("Sum2: ", sum2);
-
