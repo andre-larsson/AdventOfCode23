2760c550b2c9f9a76182fe24aace0a2268ba2d07
André Larsson <andre.larsson@paindrainer.com>
2023-12-26 12:08:13 +0100

    rewrote Day 12

--- 12/HotSprings.mjs
+++ 12/HotSprings.mjs
@@ -1,6 +1,6 @@
 import { readInput } from "../shared/util.mjs"
 
-const input = await readInput("input.data");
+const input = await readInput("sample.data");
 
 const parseInput = (input) => {
     const lines = input.split("\n")
@@ -9,7 +9,7 @@ const parseInput = (input) => {
             let [condition, nContiguous] = line.split(" ");
             nContiguous = nContiguous.split(",").map(n => parseInt(n));
             return {
-                condition,
+                condition: condition,
                 nContiguous: nContiguous
             }
         }
@@ -17,106 +17,99 @@ const parseInput = (input) => {
     return lines;
 }
 
-const conditionRecords = parseInput(input);
+const recordList = parseInput(input);
 
-const isRecordPossible = (condition, nContiguous) => {
-    // check if a record is consistent for a given number of contiguous numbers
-    let groups = condition.split(/\.+/).filter(g => g.length > 0);
-    if(groups.length != nContiguous.length) {
-        return false;
-    }
-
-    for (let i = 0; i < groups.length; i++) {
-        if (groups[i].length != nContiguous[i]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-const isPrefixPossible = (condition, nContiguous) => {
-    // check if a prefix record is consistent for a given number of contiguous
-    // numbers
-    let groups = condition.split(/\.+/).filter(g => g.length > 0);
-
-    if(groups.length <= 0) return true;
-
-    if(groups.length > nContiguous.length) {
-        return false;
-    }
-
-    // last contiguous group has an end
-    const lastGroupIsComplete = condition[condition.length-1] == ".";
-
-    for (let i = 0; i < groups.length-1; i++) {
-        if (groups[i].length != nContiguous[i]) {
-            return false;
-        }
-    }
-
-    // check last group
-    const lastGroupLength = groups[groups.length-1].length;
-    const lastContiguousLength = nContiguous[groups.length-1];
-    if (lastGroupIsComplete) {
-        if (lastGroupLength != lastContiguousLength) return false;
-    }else{
-        if (lastGroupLength > lastContiguousLength) return false;
-    }
-
-    return true;
-}
 
 const getAllPossibleRecords = (record) => {
 
-    let generated = [];
-    const generateRecords = (record, generated) => {
-        // if no ? is found, add the record, if it is consistent
-        if (!record.condition.match(/\?/)) {
-            if (isRecordPossible(record.condition, record.nContiguous)) {
-                generated.push(record);
-            }
-            return;
+    let memo = {};
+
+    const getNumPossibleArrangements = (condition, remainingGroups) => {
+        if(condition.length == 0 && remainingGroups.length == 0) {
+            // no remaining groups and no remaining condition
+            // console.log("!!!", condition, remainingGroups);
+            return 1;
+        } else if(condition.length == 0 && remainingGroups.length > 0) {
+            // not possible
+            return 0;
+        } else if(condition.length*2-1 < remainingGroups.length){
+            // cannot split into enough groups
+            return 0;
         }
 
-        const completedPart = record.condition.split("?")[0];
+        const char = condition[0];
+        const rest = condition.slice(1);
 
-        // if the completed part is not consistent, return
-        if (completedPart.length>0 && !isPrefixPossible(completedPart, record.nContiguous)) {
-            return;
+
+        let key = condition + remainingGroups.join("");
+        if(memo[key]) {
+            // console.log("memo hit", key, memo[key]);
+            return memo[key];
         }
-        
 
-        // generate all possible records for a given condition
-        // replace ? with either . or #
-        const cond1 = record.condition.replace(/\?/, "#");
-        const cond2 = record.condition.replace(/\?/, ".");
+        if(char==".") {
+            // continue
+            const result = getNumPossibleArrangements(rest, remainingGroups);
+            key = rest + remainingGroups.join("");
+            // console.log("memo miss1", key, result);
+            memo[key] = result;
+            return result;
+        }
 
-        // generate records for both conditions
-        generateRecords({
-            condition: cond1,
-            nContiguous: record.nContiguous
-        }, generated);
-        generateRecords({
-            condition: cond2,
-            nContiguous: record.nContiguous
-        }, generated);
+        // if(remainingGroups==0) {
+        //     // no more groups to fill
+        //     if( condition.match(/#/g) ) {
+        //         // no more groups but we have more # in condition
+        //         return 0;
+        //     } else {
+        //         // no more groups and no more # in condition
+        //         return 1;
+        //     }
+        // }
+
+        if( char == "#") {
+            const currentGroup = remainingGroups[0];
+            // valid if next N characters can be #, and character after that is not an #
+            if(condition.length >= currentGroup &&
+                !condition.slice(0, currentGroup).match(/\./g) &&
+                (condition.length == currentGroup || condition[currentGroup] != "#")) // character after group cannot be #
+                {
+                    const restWithoutCurrentGroup = rest.slice(currentGroup); // start next group after '.' that separates groups
+                    // console.log("condition", condition, "currentGroup", currentGroup, "restWithoutCurrentGroup", restWithoutCurrentGroup, "remainingGroups", remainingGroups);
+                    const result = getNumPossibleArrangements(restWithoutCurrentGroup, remainingGroups.slice(1))
+                    key = restWithoutCurrentGroup + remainingGroups.slice(1).join("");
+                    memo[key] = result;
+                    // console.log("memo miss2", key, result);
+                    return result;
+                }            
+            return 0;
+        }
+
+        if( char == "?") {
+            const result= getNumPossibleArrangements("#" + rest, remainingGroups)
+                + getNumPossibleArrangements("." + rest, remainingGroups);
+            memo[key] = result;
+            return result;
+        }
+        throw new Error("Invalid character");
+
     }
 
-    generateRecords(record, generated);
 
-    console.log(record.condition, generated.length);
-    return generated;
+    const result = getNumPossibleArrangements(record.condition, record.nContiguous);
+
+    console.log(result);
+    return result;
 }
 
 // Part 1
-const possibleRecords = conditionRecords.map(record => getAllPossibleRecords(record));
-const sum1 = possibleRecords.reduce((acc, records) => acc + records.length, 0);
+const possibleRecords = recordList.map(record => getAllPossibleRecords(record));
+const sum1 = possibleRecords.reduce((acc, records) => acc + records, 0);
 
 console.log(`Part 1: ${sum1}`);
 
 // Part 2
-const unfoldedRecords = conditionRecords.map(record => {
+const unfoldedRecords = recordList.map(record => {
     const unfoldedCondition = (record.condition + "?").repeat(5).slice(0, -1);
     // repeat array 5 times
     const unfoldedNContiguous = Array(5).fill(record.nContiguous).flat();
@@ -127,6 +120,7 @@ const unfoldedRecords = conditionRecords.map(record => {
 });
 
 const pr2 = unfoldedRecords.map(record => getAllPossibleRecords(record));
-const sum2 = pr2.reduce((acc, records) => acc + records.length, 0);
+const sum2 = pr2.reduce((acc, records) => acc + records, 0);
 
+// very slow and still wrong answer, am I so bad at this :(
 console.log(`Part 2: ${sum2}`);
