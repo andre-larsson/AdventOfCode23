e822440c20527d2e138afaeaf43e1108689f3297
Andre Larsson <larsson.andre@gmail.com>
2023-12-29 16:13:00 +0100

    added solution for 18:2

--- 18/LavaductLagoon.mjs
+++ 18/LavaductLagoon.mjs
@@ -34,6 +34,43 @@ const parseInputSwapped = (input) => {
     return swappedInstructions;
 }
 
+const findVertices = (instructions) => {
+
+    // we want to find the vertices that defines the outer border of the polygon
+
+    let x = 0;
+    let y = 0;
+    let vertices = [[x, y]];
+    for (const instruction of instructions) {
+        const direction = instruction[0];
+        const steps = parseInt(instruction[1]);
+
+        if (direction === "D") {
+            y += steps;
+        } else if (direction === "R") {
+            x += steps;
+        } else if (direction === "U") {
+            y -= steps;
+        } else if (direction === "L") {
+            x -= steps;
+        }
+        vertices.push([x, y]);
+    }
+    return vertices;
+}
+
+const calcArea = (P) => {
+    // shoelace formula
+
+    let area = 0;
+    const n = P.length;
+    for (let i = 1; i < n-1; i++) {
+        area += (P[i][1] + P[i+1][1])*(P[i][0] - P[i+1][0]);
+    }
+    return Math.abs(area/2);
+}
+
+
 const digBorders = (digInstructions) => {
     let map = new Set();
     let x = 0;
@@ -65,6 +102,31 @@ const digBorders = (digInstructions) => {
         }
     }
     return map;
+}
+
+const countBorders = (digInstructions) => {
+    let nBorders=0;
+    let x = 0;
+    let y = 0;
+    for (const instruction of digInstructions) {
+        const direction = instruction[0];
+        const steps = parseInt(instruction[1]);
+
+        if (direction === "D") {
+            y += steps;
+            nBorders += steps;
+        } else if (direction === "R") {
+            x += steps;
+            nBorders += steps;
+        } else if (direction === "U") {
+            y -= steps;
+            nBorders += steps;
+        } else if (direction === "L") {
+            x -= steps;
+            nBorders += steps;
+        }
+    }
+    return nBorders;
 }
 
 const fillInterior = (digged) => {
@@ -109,6 +171,24 @@ console.log("borders digged", digged1.size);
 digged1 = fillInterior(digged1);
 console.log(`Number of digged cells: ${digged1.size}`);
 
-// Part 2
+// Part 1 with shoelace formula (faster)
 
-// ????
+let vertices1 = findVertices(digInstructions1);
+
+// the vertices found by foundVertices will not always include the border tiles
+// It will only include them half of the time, (for each left border tile, there is a right border tile, 
+// and only one of them will be included in the vertices, this is how I come to understand it visually)
+// the correction is numBorders/2 + 1
+// I have no formal proof for why this correction works, but it works in all cases I tried. 
+
+const correctionTerm = countBorders(digInstructions1)/2 +1
+console.log("Area part 1:", calcArea(vertices1) + correctionTerm);
+
+// Part 2 with shoelace
+
+const digInstructions2 = parseInputSwapped(input);
+let vertices2 = findVertices(digInstructions2);
+const correctionTerm2 = countBorders(digInstructions2)/2 +1
+console.log("Area part 2:", calcArea(vertices2) + correctionTerm2);
+
+
